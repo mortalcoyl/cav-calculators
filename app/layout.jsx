6f522be51de40ad5d9d5528c834f26ca0662a6a6
Cavan Canavan <cavancanavan@gmail.com>
2026-05-10 16:26:39 -0700

    Add Vercel Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,12 +7,16 @@ export const metadata = {
   },
 };
 
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
